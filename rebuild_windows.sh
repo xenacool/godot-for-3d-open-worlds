eb08b02a2b95162346862a6d66cc4bdb9dbfb7d9
Roman Yeremenko <roal.yr@gmail.com>
2024-11-15 22:20:31 +0200

    Switch to LLVM for windows compilation

--- rebuild_windows.sh
+++ rebuild_windows.sh
@@ -1,25 +1,15 @@
 #!/bin/sh
 
 # For windows
-export MINGW32_PREFIX="/usr/bin/i686-w64-mingw32-"
-export MINGW64_PREFIX="/usr/bin/x86_64-w64-mingw32-"
+export MINGW_PREFIX="$HOME/llvm-mingw-20241030-ucrt-ubuntu-20.04-x86_64"
 
 # For incremental
 export SCONS_CACHE="./scons_cache/godot_4_scons_cache_windows";
 mkdir -p $SCONS_CACHE
 mkdir -p "./logs"
 
-${MINGW32_PREFIX}gcc --version
-${MINGW64_PREFIX}gcc --version
+${MINGW_PREFIX}/bin/x86_64-w64-mingw32-gcc --version
 
-# Cross-compiling from some Ubuntu versions may lead to this bug, 
-# due to a default configuration lacking support for POSIX threading.
-# You can change that configuration following those instructions, for 64-bit:
-#
-#   sudo update-alternatives --config x86_64-w64-mingw32-gcc
-#   <choose x86_64-w64-mingw32-gcc-posix from the list>
-#   sudo update-alternatives --config x86_64-w64-mingw32-g++
-#   <choose x86_64-w64-mingw32-g++-posix from the list>
 
 	echo
 	echo "          ==============================================="
@@ -28,11 +18,10 @@ ${MINGW64_PREFIX}gcc --version
 	echo
 
 # Editor (tools)
-scons -j2 platform=windows precision=double 2>&1 | tee ./logs/godot_4_scons_windows_64_tools_build.txt;
+scons -j2 platform=windows use_llvm=yes precision=double arch=x86_64 2>&1 | tee ./logs/godot_4_scons_windows_64_tools_build.txt;
 
 # Template(s)
-scons -j2 platform=windows precision=double target=template_debug arch=x86_64 2>&1 | tee ./logs/godot_4_scons_windows_64_template_build.txt;
-# scons -j2 platform=windows precision=double target=template_release arch=x86_64 2>&1 | tee ./logs/godot_4_scons_windows_64_template_release_build.txt;
+scons -j2 platform=windows use_llvm=yes precision=double target=template_debug arch=x86_64 2>&1 | tee ./logs/godot_4_scons_windows_64_template_build.txt;
 
 # Removing debug symbols
 #strip ./bin/godot.windows.opt.tools.64.exe
